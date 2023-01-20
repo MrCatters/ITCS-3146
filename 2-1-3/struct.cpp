eeca6d2b1e21406989bb2efb9e647710b021f41a
zachary <73203569+MrCatters@users.noreply.github.com>
2023-01-20 04:42:09 +0000

    Exercise 2-1-3: C++ Functions and Structs

---
+++ 2-1-3/struct.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+// Creates the CartesianCoordinate structure.
+struct CartesianCoordinate{
+	float x;
+	float y;
+};
+
+// Calculates the distance between two points on a graph.
+float calculateDistance(CartesianCoordinate *pointOne, CartesianCoordinate *pointTwo){
+	float xCalc = pow(((*pointTwo).x - (*pointOne).x), 2);
+	float yCalc = pow(((*pointTwo).y - (*pointOne).y), 2);
+	return sqrt(xCalc + yCalc);
+}
+
+// Prompts the user for two coordinates and outputs their distance.
+int main() {
+	CartesianCoordinate p1, p2;
+	cout << "Enter the first x coordinate:" << endl;
+	cin >> p1.x;
+	cout << "Enter the first y coordinate:" << endl;
+	cin >> p1.y;
+	cout << "Enter the second x coordinate:" << endl;
+	cin >> p2.x;
+	cout << "Enter the second y coordinate:" << endl;
+	cin >> p2.y;
+	cout << calculateDistance(&p1, &p2);
+}

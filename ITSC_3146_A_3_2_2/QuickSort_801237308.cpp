463d51f828b1b3aa99a7906b94b656b73523034d
zachary <73203569+MrCatters@users.noreply.github.com>
2023-01-24 01:38:15 +0000

    module 3 completed

--- ITSC_3146_A_3_2_2/QuickSort_801237308.cpp
+++ ITSC_3146_A_3_2_2/QuickSort_801237308.cpp
@@ -6,10 +6,11 @@
 //  Copyright © 2019 Bahamon, Julio. All rights reserved.
 //
 
+// Zachary Palko
+
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
-
 using namespace std;
 
 //  Declaring a new struct to store patient data
@@ -19,6 +20,7 @@ struct patient {
     float balance;
 };
 
+// Displays the contents of the patient list with commas between fields.
 void displayPatientList(struct patient patients[]){
 	for(int i = 0; i < 6; i++){
 		cout << patients[i].age << ", ";
@@ -27,24 +29,18 @@ void displayPatientList(struct patient patients[]){
 	}
 }
 
-//  TODO:
-//  IMPLEMENT A FUNCTION THAT COMPARES TWO PATIENTS BY AGE
-
-//  THE FUNCTION RETURNS AN INTEGER AS FOLLOWS:
-//      -1 IF THE AGE OF THE FIRST PATIENT IS LESS
-//         THAN THE SECOND PATIENT'S AGE
-//       0 IF THE AGES ARE EQUAL
-//       1 OTHERWISE
-// https://stackoverflow.com/questions/37881240/what-is-const-void
-// https://stackoverflow.com/questions/6105513/need-help-using-qsort-with-an-array-of-structs
-// `const void`
-	// Const stands for constant. 
-	// void means the pointer can hold data of any type.
+// https://cplusplus.com/doc/tutorial/pointers/
+// Compares two patients by age.
+// Returns -1 if the age of the first patient is less than the second patient's age.
+// Returns 0 if the ages are equal.
+// Returns 1 otherwise.
 int comparePatientsByAge(const void *patient1, const void *patient2){
 	
+    // Assigns the age field of the patient struct to an int variable.
 	int patientAge1 = ((struct patient *)patient1) -> age;
 	int patientAge2 = ((struct patient *)patient2) -> age;
 		
+    // Compares the two ages and returns the result.
 	if (patientAge1 < patientAge2) {
 		return -1;
 	} else if (patientAge1 == patientAge2) {
@@ -57,27 +53,50 @@ int comparePatientsByAge(const void *patient1, const void *patient2){
 
 
 
-//  TODO:
-//  IMPLEMENT A FUNCTION THAT COMPARES TWO PATIENTS BY BALANCE DUE
+// Compares two patients by balance.
+// Returns -1 if the balance of the first patient is less than the second patient's balance.
+// Returns 0 if the balances are equal.
+// Returns 1 otherwise.
+int comparePatientsByBalance(const void *patient1, const void *patient2){
+	
+    // Assigns the balance field of the patient struct to a float variable.
+	float patientBalance1 = ((struct patient *)patient1) -> balance;
+	float patientBalance2 = ((struct patient *)patient2) -> balance;
+	
+    // Compares the two balances and returns the result.
+	if (patientBalance1 < patientBalance2) {
+		return -1;
+	} else if (patientBalance1 == patientBalance2) {
+		return 0;
+	} else {
+		return 1;
+	}
+	
+}
 
-//  THE FUNCTION RETURNS AN INTEGER AS FOLLOWS:
-//      -1 IF THE BALANCE FOR THE FIRST PATIENT IS LESS
-//         THAN THE SECOND PATIENT'S BALANCE
-//       0 IF THE BALANCES ARE EQUAL
-//       1 OTHERWISE
 
+// http://www.cplusplus.com/reference/cstring/strncmp/
+// Compares two patients by name.
+// Returns -1 if the name of the first patient is less than the second patient's name.
+// Returns 0 if the ages are equal.
+// Returns 1 otherwise.
+int comparePatientsByName(const void *patient1, const void *patient2){
+	// Assigns the address of the name field of the patient struct to a void pointer.
+    void *patientName1 = &((struct patient *)patient1) -> name;
+	void *patientName2 = &((struct patient *)patient2) -> name;
+	
+    // Compares the two names and returns the result.
+	int relation = strncmp((char*)patientName1, (char*)patientName2, 20);
+	if (relation < 0) {
+		return -1;
+	} else if (comparePatientsByAge(patient1, patient2) == 0) {
+		return 0;
+	} else {
+		return 1;
+	}
+	
+}
 
-//  TODO:
-//  IMPLEMENT A FUNCTION THAT COMPARES TWO PATIENTS BY NAME
-
-//  THE FUNCTION RETURNS AN INTEGER AS FOLLOWS:
-//      -1 IF THE NAME OF THE FIRST PATIENT GOES BEFORE
-//         THE SECOND PATIENT'S NAME
-//       0 IF THE AGES ARE EQUAL
-//       1 OTHERWISE
-//
-//  HINT: USE THE strncmp FUNCTION
-//  (SEE http://www.cplusplus.com/reference/cstring/strncmp/)
 
 
 //  The main program
@@ -85,7 +104,7 @@ int main()
 {
     
     //  Storing some test data
-    struct patient patient_list[5] = {
+    struct patient patient_list[6] = {
         {25, "Juan Valdez   ", 1250},
         {15, "James Morris  ", 200},
         {32, "Tyra Banks    ", 750},
@@ -93,6 +112,8 @@ int main()
         {53, "Pablo Picasso ", 615}
     };
     
+
+    // Prompts the user to enter data for a new patient
     cout << "Enter your last name: ";
     cin >> patient_list[5].name;
     cout << endl;
@@ -103,48 +124,35 @@ int main()
     cin >> patient_list[5].balance;
     cout << endl;
     cout << "Patient List: " << endl;
+
+    // Displays the contents of the array
     displayPatientList(patient_list);
     cout << endl;
+
     
-    
+    // Sorts the array by age and display the results
     cout << "Sorting..." << endl;
-    
-    //  TODO:
-    //  CALL THE qsort FUNCTION TO SORT THE ARRAY BY PATIENT AGE
     qsort(patient_list, 6, sizeof(patient), comparePatientsByAge);
+
     cout << "Patient List - Sorted by Age: " << endl;
-    
-    //  TODO:
-    //  DISPLAY THE CONTENTS OF THE ARRAY
-    //  AFTER SORTING BY AGE
     displayPatientList(patient_list);
     cout << endl;
     
-    
+
+    // Sorts the array by balance and display the results
     cout << "Sorting..." << endl;
-    
-    //  TODO:
-    //  CALL THE qsort FUNCTION TO SORT THE ARRAY BY PATIENT BALANCE
-    
     cout << "Patient List - Sorted by Balance Due: " << endl;
-    
-    //  TODO:
-    //  DISPLAY THE CONTENTS OF THE ARRAY
-    //  AFTER SORTING BY BALANCE
+    qsort(patient_list, 6, sizeof(patient), comparePatientsByBalance);
+
     displayPatientList(patient_list);
     cout << endl;
     
     
+    // Sorts the array by name and display the results
     cout << "Sorting..." << endl;
-    
-    //  TODO:
-    //  CALL THE qsort FUNCTION TO SORT THE ARRAY BY PATIENT NAME
-    
+    qsort(patient_list, 6, sizeof(patient), comparePatientsByName);
     cout << "Patient List - Sorted by Name: " << endl;
-    
-    //  TODO:
-    //  DISPLAY THE CONTENTS OF THE ARRAY
-    //  AFTER SORTING BY NAME
+
     displayPatientList(patient_list);
     cout << endl;
     
